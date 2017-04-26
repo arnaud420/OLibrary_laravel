52883ee97da28d3b945d4e55dba01ade2c98307a
arnaud420 <arnaud_lafon@hotmail.fr>
2017-04-26 18:13:29 +0200

    vue exemplaires

--- olibrary/resources/views/front/exemplaires.blade.php
+++ olibrary/resources/views/front/exemplaires.blade.php
@@ -1,7 +1,12 @@
-@extends('commons.template')
+@extends('commons.default')
 
-@section('titre')
-    Exemplaires
+@section('title', 'Exemplaires')
+
+@section('styles')
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 @endsection
 
 @section('content')
@@ -9,7 +14,7 @@
         <div class="col s12 m2">
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" style="height: 150px; width: 150px; margin: 0 auto" src="http://lelivrequifaitdireoui.quebec/resources/livre.png">
+                    <img class="activator responsive-img" src="http://lelivrequifaitdireoui.quebec/resources/livre.png">
                 </div>
                 <div class="card-content">
                     <span class="card-title activator center-align" id="titre-livre">Titre</span>
@@ -21,5 +26,9 @@
             </div>
         </div>
     </div>
+@endsection
 
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
 @endsection
