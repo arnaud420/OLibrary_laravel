8a2371b4d27819b0cbccd1a6355157f8745a0804
arnaud420 <arnaud_lafon@hotmail.fr>
2017-04-27 16:23:39 +0200

    responsive menu navbar

--- olibrary/resources/views/commons/navbar.blade.php
+++ olibrary/resources/views/commons/navbar.blade.php
@@ -6,5 +6,16 @@
       <li><a href="#">Emprunts</a></li>
       <li><a href="#">Catalogue</a></li>
     </ul>
+
+
+    <!-- menu mobile tablette  -->
+    <a href="#" data-activates="mobile-demo" class="button-collapse right"><i class="material-icons">menu</i></a>
+    <ul class="side-nav" id="mobile-demo">
+      <li class="item"><a href="">Profil</a></li>
+      <li class="item"><a href="">Catalogue</a></li>
+      <li class="item"><a href="">Espace Personnel</a></li>
+      <li class="item"><a href="">Emprunts</a></li>
+      <li class="item"><a href="">Déconnexion</a></li>
+    </ul>
   </div>
 </nav>
