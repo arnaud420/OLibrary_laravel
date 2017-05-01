02ec62a00d42f5ed47eba717d754d33b5088a7f0
arnaud420 <arnaud_lafon@hotmail.fr>
2017-05-01 17:48:58 +0200

    view profil

--- olibrary/resources/views/user/profile.blade.php
+++ olibrary/resources/views/user/profile.blade.php
@@ -8,15 +8,25 @@
 @endsection
 
 @section('content')
-  <h1>Profile</h1>
-  <p>Firstname: {{ $user->first_name }}</p>
-  <p>Lastname: {{ $user->last_name }}</p>
-  <p>Email: {{ $user->email }}</p>
-  <p>Phone: {{ $user->phone }}</p>
-  <p>Address: {{ $user->address }}</p>
-  <p>City: {{ $user->city }}</p>
-  <p>Postal code: {{ $user->postal_code }}</p>
-  <p>picture_path: {{ $user->picture_path }}</p>
+  <h1 class="center">Profile</h1>
+
+  <div class="row">
+    <div class="col s12">
+      <img src="{{$user->picture_path}}" style="width: 200px; height: 180px; float: left; margin-right: 20px;">
+
+      <p>Prénom : {{ $user->first_name }}</p>
+      <p>Nom : {{ $user->last_name }}</p>
+      <p>Email: {{ $user->email }}</p>
+      <p>Téléphone : {{ $user->phone }}</p>
+    </div>
+
+    <div class="col s12">
+      <h4>Adresse</h4>
+      <p>Rue : {{ $user->address }}</p>
+      <p>Ville : {{ $user->city }}</p>
+      <p>Code postal : {{ $user->postal_code }}</p>
+    </div>
+  </div>
 
   <div class="row center">
     <a class="waves-effect waves-light btn-large red" href="{{route('user.edit', compact('user'))}}">Modifier le profil</a>
