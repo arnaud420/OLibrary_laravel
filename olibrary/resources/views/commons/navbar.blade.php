0e4dea20549431297889b6e74e381b9a8f919084
tonychg <antoine.chiny@gmail.com>
2017-05-09 13:42:20 +0200

    Add svg icon + navbar logo (not functionnal)

--- olibrary/resources/views/commons/navbar.blade.php
+++ olibrary/resources/views/commons/navbar.blade.php
@@ -1,6 +1,8 @@
 <nav id="navbar" class="indigo darken-1">
   <div class="nav-wrapper">
-    <a href="/" class="brand-logo">OLibrary</a>
+    <a href="/" class="brand-logo">
+      <img id="logo" src="uploads/open-book.svg">
+    </a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
       @if(!Auth::check())
         <li><a href="/login">Connexion</a></li>
